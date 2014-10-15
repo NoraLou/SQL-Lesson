79bdddc10de9836a79f4d733b616be8fee39db49
Hackbright Student <info@hackbrightacademy.com>
2014-10-14 18:00:39 -0700

    Got half way through part 3 and things are still working

--- webapp.py
+++ webapp.py
@@ -4,11 +4,14 @@ import hackbright_app
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/student/")
 def get_student():
     hackbright_app.connect_to_db()
     student_github = request.args.get("student")
-    return hackbright_app.get_student_by_github(student_github)
+    row = hackbright_app.get_student_by_github(student_github)
+    html = render_template("student_info.html", first_name=row[0],
+        last_name=row[1], github=row[2])
+    return html
 
 if __name__ == "__main__":
     app.run(debug=True)
