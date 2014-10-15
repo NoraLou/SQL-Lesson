5d55028d89e7d0afa27e5fbb30f9dc99d865aeeb
Hackbright Student <info@hackbrightacademy.com>
2014-10-14 17:06:53 -0700

    added expaned queries

--- hackbright_app.py
+++ hackbright_app.py
@@ -8,8 +8,51 @@ def get_student_by_github(github):
     DB.execute(query, (github,))
     row = DB.fetchone()
     print """\
-Student: %s %s
-Github account: %s"""%(row[0], row[1], row[2])
+    Project: %s %s
+    Github account: %s"""%(row[0], row[1], row[2])
+
+def get_project_by_title(title):
+    query = """SELECT title, description, max_grade FROM Projects WHERE title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    print """\
+    Project: %s 
+    Description: %s
+    Max Grade: %s""" %(row[0], row[1], row[2])
+
+def get_grade_by_project(title):
+    query = """SELECT Students.first_name, Projects.title, Grades.grade 
+    FROM Students INNER JOIN Grades ON (Students.github= Grades.student_github) 
+    INNER JOIN Projects ON (Grades.project_title = Projects.title) 
+    WHERE title = ?"""
+    for row in DB.execute(query, (title,)):
+    #  row = DB.fetchone()
+        print """\
+        First Name: %s 
+        Project Title: %s
+        Student Grade: %s""" %(row[0], row[1], row[2])
+
+def get_grades_by_first_name(first_name):
+    query = """SELECT Students.first_name, Projects.title, Grades.grade 
+    FROM Students INNER JOIN Grades ON (Students.github= Grades.student_github) 
+    INNER JOIN Projects ON (Grades.project_title = Projects.title) 
+    WHERE Students.first_name = ?"""
+    
+    for row in DB.execute(query, (first_name,)):
+        print """\
+        First Name: %s 
+        Project Title: %s
+        Student Grade: %s""" %(row[0], row[1], row[2])
+       
+
+
+    # rows = DB.execute(query, (first_name,))
+    # for row in rows:
+    #     row = DB.fetchone()
+    #     print """\
+    #     First Name: %s 
+    #     Project Title: %s
+    #     Student Grade: %s""" %(row[0], row[1], row[2])
 
 def connect_to_db():
     global DB, CONN
@@ -20,8 +63,15 @@ def main():
     connect_to_db()
     command = None
     while command != "quit":
-        input_string = raw_input("HBA Database > Type student and github to do a lookup, or new_student and first, last github to add a student.  ")
-        tokens = input_string.split()
+        input_string = raw_input("""HBA Database > 
+            Type student and github to do a lookup, 
+            OR new_student and first, last, github to add a student,  
+            OR title, followed by project title for project details,
+            OR enter 'new_project' followed by: project title, description, and max_grade to create new project
+            OR enter 'title_grade' to get student grades by project title  
+            OR enter 'new_grade', github, project title, grade to enter a new grade for a student 
+            OR enter 'get_grades' and enter first name of a student to view all their grades  """)
+        tokens = input_string.split(",")
         command = tokens[0]
         args = tokens[1:]
 
@@ -29,12 +79,34 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "title":
+            get_project_by_title(*args)
+        elif command == "new_project":
+            make_new_project(*args)
+        elif command == "title_grade":
+            get_grade_by_project(*args)
+        elif command == "new_grade":
+            make_new_grade (*args)
+        elif command == "get_grades":
+            get_grades_by_first_name(*args)
 
 def make_new_student(first_name, last_name, github):
     query = """INSERT into Students values (?, ?, ?)"""
     DB.execute(query, (first_name, last_name, github))
     CONN.commit()
     print "Successfully added student: %s %s"%(first_name, last_name)
+
+def make_new_project(title, description, max_grade):
+    query = """INSERT into Projects (title,description,max_grade) values (?, ?, ?)"""
+    DB.execute(query,(title, description, max_grade))
+    CONN.commit()
+    print "Successfully added project: %s %s %s"%(title, description, max_grade)
+
+def make_new_grade(student_github, project_title, grade):
+    query = """INSERT into Grades values (?, ?, ?)"""
+    DB.execute(query, (student_github, project_title, grade))
+    CONN.commit()
+    print "Successfully added grade: %s %s %s"%(student_github, project_title, grade)
 
 
     CONN.close()
